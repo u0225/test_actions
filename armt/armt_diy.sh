2f694ecb344a969c313e81e576a2912f57ef2dbe
u0225 <qq3842126@gmail.com>
2020-05-26 05:50:59 +0800

    update r20

---
+++ armt/armt_diy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 本脚本工作目录必须是git clone的主目录
+# x86_64
+
+# Add some default settings
+#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+
+# # curl -fsSL https://raw.githubusercontent.com/u0225/actions-openwrt-lede/master/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
+
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$0CVWiweD$4Xsq83ZIZtPJe8PVLfqJH0:16357:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/sed -i 's\/downloads.openwrt.org/# sed -i 's\/downloads.openwrt.org/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/sed -i 's\/http\/https/# sed -i 's\/http\/https/g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/exit 0/echo 0xDEADBEEF > \/etc\/config\/google_fu_mode/g' package/lean/default-settings/files/zzz-default-settings
+echo "" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba4.lua" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/invalid users = root/# invalid users = root/g' /etc/samba/smb.conf.template" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/'192.168.1.1'/'192.168.10.1'/g' /etc/config/network" >> package/lean/default-settings/files/zzz-default-settings
+# echo 'echo "client min protocol = SMB2" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+# echo 'echo "client max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+# echo 'echo "min protocol = NT1" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+# echo 'echo "max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+echo "" >> package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
