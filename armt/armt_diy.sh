39e96c2bd68b104c4bfa4b65a6397b5824cd0db5
u0225 <qq3842126@gmail.com>
2020-05-27 04:23:01 +0800

    update r20

--- armt/armt_diy.sh
+++ armt/armt_diy.sh
@@ -15,9 +15,9 @@ echo "" >> package/lean/default-settings/files/zzz-default-settings
 echo "sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba4.lua" >> package/lean/default-settings/files/zzz-default-settings
 echo "sed -i 's/invalid users = root/# invalid users = root/g' /etc/samba/smb.conf.template" >> package/lean/default-settings/files/zzz-default-settings
 echo "sed -i 's/'192.168.1.1'/'192.168.10.1'/g' /etc/config/network" >> package/lean/default-settings/files/zzz-default-settings
-# echo 'echo "client min protocol = SMB2" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
-# echo 'echo "client max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
-# echo 'echo "min protocol = NT1" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
-# echo 'echo "max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+echo 'echo "client min protocol = SMB2" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+echo 'echo "client max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+echo 'echo "min protocol = NT1" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
+echo 'echo "max protocol = SMB3" >> /etc/samba/smb.conf.template' >> package/lean/default-settings/files/zzz-default-settings
 echo "" >> package/lean/default-settings/files/zzz-default-settings
 echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
