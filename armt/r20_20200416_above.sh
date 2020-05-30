c662bbb9fb2852ca48411f6d54e26ba456d09296
u0225 <qq3842126@gmail.com>
2020-05-30 09:07:43 +0800

    update master

--- armt/r20_20200416_above.sh
+++ armt/r20_20200416_above.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #
-# R20 20200101-20200416 r20.4.8-20200408 r20.4.8-20200416
+# R20 20200416 之后 r20.5.20 master  20200416之后的2SR因脱离需要整理
 #
+
+#
+# 下面这项是为r20.5.20或者是20200416之后源码使用的，之前的不能用
+#
+sed -i s"/#src-git helloworld/src-git helloworld/g" feeds.conf.default
+
 cd armt
 echo "sed -i 's/reg = <0x50000 0x7b0000>;/reg = <0x50000 0xfb0000>;/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-wr8305rt.dts" >> armt_set.sh
 echo "CONFIG_TARGET_ramips_mt7620_DEVICE_xiaomi_miwifi-mini=y" >> miwifi_mini_16m
--- armt/r89.sh
+++ armt/r89.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
-# R8-9
+# R8-9 r8.1.12 r9.8.17 r9.10.10
 #
+
+# r8-r9
+sed -i s"/openwrt-19.07/openwrt-18.06/g" feeds.conf.default
+
 cd armt
 echo "sed -i 's/reg = <0x50000 0x7b0000>;/reg = <0x50000 0xfb0000>;/g' target/linux/ramips/dts/ZBT-WR8305RT.dts" >> armt_set.sh
 echo "CONFIG_TARGET_ramips_mt7620_DEVICE_miwifi-mini=y" >> miwifi_mini_16m
