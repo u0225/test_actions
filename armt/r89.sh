ad612c223313fae0fef9bc05bed20b8ae82bb691
u0225 <qq3842126@gmail.com>
2020-05-31 09:22:22 +0800

    update master

--- armt/r89.sh
+++ armt/r89.sh
@@ -5,10 +5,10 @@
 
 # =========================================================================================
 # 这项是为r8-9(r8.1.12 r9.8.17 r9.10.10)或者是20200416(master)之前源码使用的，之后的不能用 #
-cd ..
 sed -i s"/openwrt-19.07/openwrt-18.06/g" feeds.conf.default
 #                                                                                          #
 
+cd ..
 cd armt
 echo "sed -i 's/reg = <0x50000 0x7b0000>;/reg = <0x50000 0xfb0000>;/g' target/linux/ramips/dts/ZBT-WR8305RT.dts" >> armt-set.sh
 echo "CONFIG_TARGET_ramips_mt7620_DEVICE_miwifi-mini=y" >> miwifi-mini-16m
