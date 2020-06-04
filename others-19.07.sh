9ecce8d33d359ca42bb095a36a15502113ea4584
u0225 <qq3842126@gmail.com>
2020-06-04 09:09:28 +0800

    update master

---
+++ others-19.07.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------------------------------
+#
+
+cd package
+mkdir others-packages
+cd others-packages
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+# git clone https://github.com/Aslin-Ameng/luci-theme-Night.git
+# git clone https://github.com/garypang13/luci-app-eqos.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/tty228/luci-app-serverchan.git
