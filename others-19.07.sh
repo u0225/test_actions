c1e93c4f274b6b2699d27afe211188466cae7ca9
u0225 <qq3842126@gmail.com>
2020-06-04 11:15:34 +0800

    update master

--- others-19.07.sh
+++ others-19.07.sh
@@ -3,8 +3,6 @@
 #
 
 cd package
-mkdir others-packages
-cd others-packages
 # git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
 # git clone https://github.com/Aslin-Ameng/luci-theme-Night.git
 # git clone https://github.com/garypang13/luci-app-eqos.git
