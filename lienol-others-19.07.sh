29727d05dc89a65bf2dae8a5b670b2d12cd7aba2
u0225 <qq3842126@gmail.com>
2020-06-08 16:01:36 +0800

    update master

---
+++ lienol-others-19.07.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -----------------------------------------------------------
+#
+
+mkdir -p package/openwrt-others
+cd package/openwrt-others
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+# git clone https://github.com/Aslin-Ameng/luci-theme-Night.git
+# git clone https://github.com/garypang13/luci-app-eqos.git
+#
+git clone https://github.com/fw876/helloworld.git
+git clone https://github.com/u0225/psw_single.git
+git clone https://github.com/u0225/OpenClash.git
+git clone https://github.com/u0225/luci-app-aliddns.git
+git clone https://github.com/u0225/luci-app-dnspod.git
+git clone https://github.com/u0225/luci-app-smartdns.git
+git clone https://github.com/u0225/smartdns.git
+git clone https://github.com/u0225/luci-app-eqos.git
+#
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/tty228/luci-app-serverchan.git
