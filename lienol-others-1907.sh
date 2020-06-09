5b70bda95edf876bc05a2431a73ec1435f4d70b0
u0225 <qq3842126@gmail.com>
2020-06-09 15:35:03 +0800

    update master

--- lienol-others-1907.sh
+++ lienol-others-1907.sh
@@ -3,10 +3,20 @@
 #
 
 cd package
-rm -rf package/lean/luci-app-dockerman
-rm -rf package/lean/smartdns
+rm -rf package/diy
+rm -rf package/lean
+rm -rf package/OpenAppFilter
 rm -rf package/default-settings/files/zzz-default-settings
+
+cd ..
+mv -f package-others/zzz-default-settings openwrt/package/default-settings/files
+mv -rf package-others/* openwrt/package
+
+cd openwrt/package/lean/ssrplus
 git clone https://github.com/fw876/helloworld.git
+
+cd .. && cd ..
+cd others
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git
 # git clone https://github.com/destan19/OpenAppFilter.git
@@ -15,12 +25,8 @@ git clone https://github.com/lisaac/luci-app-dockerman
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
 git clone https://github.com/Aslin-Ameng/luci-theme-Night.git
 git clone https://github.com/garypang13/luci-app-eqos.git
-cd .. && cd ..
-mv -f package-others/zzz-default-settings openwrt/package/default-settings/files
-cp -rf package-others/* openwrt/package
-#
+
 # git clone https://github.com/u0225/package-others.git
-#
 # git clone https://github.com/u0225/psw_single.git
 # git clone https://github.com/u0225/OpenClash.git
 # git clone https://github.com/u0225/luci-app-aliddns.git
