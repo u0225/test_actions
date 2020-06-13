74dcce61cbb548cd94cde90279d540a9bf75987b
u0225 <qq3842126@gmail.com>
2020-06-13 23:00:08 +0800

    update master

--- armt/master.sh
+++ armt/master.sh
@@ -7,9 +7,8 @@
 # =======================================================================
 # 下面这项是为r20.5.20或者是20200416(master)之后源码使用的，之前的不能用 #
 #                                                                        #
-sed -i s"/#src-git helloworld/src-git helloworld/g" feeds.conf.default
+sed -i s"/#src-git helloworld/src-git helloworld/g" lede/feeds.conf.default
 
-cd ..
 cd armt
 echo "sed -i 's/reg = <0x50000 0x7b0000>;/reg = <0x50000 0xfb0000>;/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-wr8305rt.dts" >> armt-set.sh
 echo "CONFIG_TARGET_ramips_mt7620_DEVICE_xiaomi_miwifi-mini=y" >> miwifi_mini-16m
--- armt/r20.sh
+++ armt/r20.sh
@@ -4,7 +4,6 @@
 # R20与R8-9设置文件名称不同需要整理 R20 20200101-20200416 r20.4.8-20200408 r20.4.8-20200416
 #
 
-cd ..
 cd armt
 echo "sed -i 's/reg = <0x50000 0x7b0000>;/reg = <0x50000 0xfb0000>;/g' target/linux/ramips/dts/mt7620n_zbtlink_zbt-wr8305rt.dts" >> armt-set.sh
 echo "CONFIG_TARGET_ramips_mt7620_DEVICE_xiaomi_miwifi-mini=y" >> miwifi-mini-16m
